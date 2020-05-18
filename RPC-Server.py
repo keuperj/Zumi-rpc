c46e333b20bf9105057d6dc86c4f0e8b05fa6e23
zumi <zumi@robolink.com>
2020-05-18 17:51:31 +0100

    Server added

--- RPC-Server.py
+++ RPC-Server.py
@@ -4,6 +4,7 @@ from rpyc.utils.authenticators import SSLAuthenticator
 import ssl
 import os
 import datetime
+from zumi.zumi import Zumi
 
 DEBUG=True
 PORT=9004
@@ -17,16 +18,21 @@ class ZumiService(rpyc.Service):
     def __init__(self):
         self.myConnection = ''
         self.user = ''
+        #init zumi instance 
+        self.zumi = Zumi()
 
     def on_connect(self, conn):
         """ method is automatically run at new rpc connections
         """
         self.myConnection = conn
-        print (conn._config['credentials']['subject'])
-        self.user = conn._config['credentials']['subject'][5][0][1]
-        if DEBUG:
-            print("new connection: ",
-                  conn._config['endpoints'], "from user ", self.user)
+        print ('new connection')
+
+        #print (conn._config['credentials']['subject'])
+        #self.user = conn._config['credentials']['subject'][5][0][1]
+        #if DEBUG:
+            #print("new connection: ",
+            #      conn._config['endpoints'], "from user ", self.user)
+        
         pass
 
     def on_disconnect(self, conn):
@@ -44,10 +50,17 @@ class ZumiService(rpyc.Service):
         return 'pong'
 
 
+    def exposed_forward(self):
+        self.zumi.forward()
+
+
+
+
+
 if __name__ == "__main__":
     print("Zumi RPC Server up ")
     auth = SSLAuthenticator("zumi_rpc.key", 
                             "zumi_rpc.crt", cert_reqs=ssl.CERT_REQUIRED, ca_certs="zumi_rpc.crt")
-    server = ThreadedServer(ZumiService, port=PORT,
-                            authenticator=auth, protocol_config={'allow_all_attrs': True})
+    server = ThreadedServer(ZumiService, port=PORT,  protocol_config={'allow_all_attr':True})
+                            #authenticator=auth, protocol_config={'allow_all_attrs': True})
     server.start()
